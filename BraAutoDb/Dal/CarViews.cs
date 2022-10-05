bf8e355c723e370d5908785c29c1a36ae8523014
Dimitur <dimitur.mihailov988@gmail.com>
2022-10-05 23:11:58 +0300

    fix

---
+++ BraAutoDb/Dal/CarViews.cs
@@ -0,0 +1,53 @@
+﻿using BraAutoDb.Models;
+
+namespace BraAutoDb.Dal
+{
+    public class CarViews : BaseDal<CarView>
+    {
+        public CarViews() : base("car_view", "id", "car_id") { }
+
+        public CarView Get(uint carId, string userIp)
+        {
+            var sql = @"
+                SELECT *
+                FROM car_view 
+                WHERE car_id = @carId AND user_ip = @userIp";
+
+            return Db.Mapper.Query<CarView>(sql, new { carId, userIp }).FirstOrDefault();
+        }
+
+        public int GetCountByCarId(uint carId)
+        {
+            var sql = @"
+                SELECT COUNT(*) 
+                FROM car_view 
+                WHERE car_id = @carId";
+
+            return Db.Mapper.Query<int>(sql, new { carId }).FirstOrDefault();
+        }
+
+        public void Insert(CarView carView)
+        {
+            var sql = @"INSERT INTO `car_view`
+                        (
+                            `car_id`,
+                            `user_ip`,
+                            `created_at`
+                        )VALUES(
+                            @carId,
+                            @userIp,
+                            NOW()
+                        );
+
+                        SELECT LAST_INSERT_ID() AS id;";
+
+            var queryParams = new
+            {
+                carId = carView.CarId,
+                userIp = carView.UserIp,
+            };
+
+            carView.Id = Db.Mapper.Query<uint>(sql, queryParams).FirstOrDefault();
+        }
+    }
+}
---
+++ BraAutoDb/Models/CarView.cs
@@ -0,0 +1,17 @@
+﻿using DapperMySqlMapper;
+
+namespace BraAutoDb.Models
+{
+    [Table(Name = "car_view")]
+    public class CarView : BaseModel<uint>
+    {
+        [Column(Name = "car_id")]
+        public uint CarId { get; set; }
+
+        [Column(Name = "user_ip")]
+        public string UserIp { get; set; }
+
+        [Column(Name = "created_at")]
+        public DateTime CreatedAt { get; set; }
+    }
+}
